c2e0bf705d1a35a19e7c8b09b948b74464dd31f5
ChenJiagui <20302010002@fudan.edu.cn>
2023-06-12 19:12:25 +0800

    添加目标统计信息

--- pose/pose.py
+++ pose/pose.py
@@ -6,6 +6,7 @@ import cv2
 from pathlib import Path
 import os
 import math
+from PIL import ImageFont, ImageDraw, Image
 
 from videosource import FileSource, WebcamSource
 def cal_angle(ankle, knee, hip):
@@ -23,13 +24,11 @@ def cal_angle(ankle, knee, hip):
 
 def getSubFrame(frame, xyxy):
     x1, y1, x2, y2 = xyxy
-    # print(frame.copy()[y1:y2, x1:x2].shape)
     return frame.copy()[y1:y2, x1:x2]
 
 def drawSubFrame(frame, sub_frame, xyxy):
     x1, y1, x2, y2 = xyxy
     frame[y1:y2, x1:x2] = sub_frame
-    # print(frame[y1:y2, x1:x2].shape)
     return frame
 
 def cal_cycler(isCycler):
@@ -39,29 +38,51 @@ def cal_cycler(isCycler):
             cnt += 1
     return cnt
 
-def addInfoText(frame, isCycler):
-    font = cv2.FONT_HERSHEY_SIMPLEX
+def addInfoText(frame, cycler_cnt, person_cnt, bicycle_cnt, motorcycle_cnt):
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    font = ImageFont.truetype("SimSun.ttf", 30, encoding="utf-8")
     font_scale = 1
-    font_color = (255, 255, 255)  # 白色
+    font_color = (99, 28, 241)  # 紫色
     line_type = 2
     
-    cycler_cnt = 0
-    people_cnt = 0
-    for item in isCycler:
-        if item[1]:
-            cycler_cnt += 1
-        people_cnt += 1
+    # text = f"people number : {cycler_cnt}\n" + f"cycler number : {cycler_cnt}\n"
+    # text_size, _ = cv2.getTextSize(text, font, font_scale, line_type)
+    # text_x = frame.shape[1] - text_size[0] - 10  # 10 是文字与右边界的距离
+    # text_y = text_size[1] + 10  # 10 是文字与顶部边界的距离 
+    # cv2.putText(frame, text, (text_x, text_y), font, font_scale, font_color, line_type)
     
-    text = f"people number : {cycler_cnt}\n" + f"cycler number : {cycler_cnt}\n"
-    text_size, _ = cv2.getTextSize(text, font, font_scale, line_type)
-    text_x = frame.shape[1] - text_size[0] - 10  # 10 是文字与右边界的距离
-    text_y = text_size[1] + 10  # 10 是文字与顶部边界的距离 
-    cv2.putText(frame, text, (text_x, text_y), font, font_scale, font_color, line_type)
+    row1 = f"总人数: {person_cnt}"
+    row2 = f"走路: {person_cnt-cycler_cnt}"
+    row3 = f"骑行: {cycler_cnt}"
+
+    row4 = f"总车数: {bicycle_cnt+motorcycle_cnt}"
+    row5 = f"自行车: {bicycle_cnt}"
+    row6 = f"单人摩托: {bicycle_cnt+2*motorcycle_cnt-cycler_cnt}"
+    row7 = f"双人摩托: {cycler_cnt-bicycle_cnt-motorcycle_cnt}"
+
 
     
-    
-    
-    
+    row_list = [row1, row2, row3, row4, row5, row6, row7]
+    text_size_list = []
+    for row in row_list:
+        # print(row)
+        # text_size, _ = cv2.getTextSize(row, font, font_scale, line_type)
+        text_size = font.getsize(row)
+        text_size_list.append(text_size)
+        
+    for i in range(len(row_list)):
+        row = row_list[i]
+        tx = text_size_list[i][0]
+        ty = text_size_list[i][1]
+
+        x = 0.99 * frame.shape[1] - tx
+        y = 0.01 * frame.shape[0] - ty * 0.2 + ty * 1.1 * i
+
+        frame_pil = Image.fromarray(frame)  #转为PIL的图片格式
+        draw = ImageDraw.Draw(frame_pil)
+        ImageDraw.Draw(frame_pil).text((int(x), int(y)), row, font_color, font)
+        frame = np.array(frame_pil)
+    return frame
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -110,8 +131,12 @@ def main(inp, mylabels_path='video_obj_detect'):
         # 遍历每一帧
         for idx, (frame, frame_rgb) in enumerate(source):
             # txt_path = f"../yolov5/myfolder/mylabels/test_{idx+1}.txt"
-            txt_path = f"{base_folder}/mylabels/test_{idx+1}.txt"
+            txt_path = f"{base_folder}/mylabels/{p.stem}_{idx+1}.txt"
             objs = []
+
+            person_cnt = 0
+            bicycle_cnt = 0
+            motorcycle_cnt = 0
             if os.path.exists(txt_path):
                 with open(txt_path, 'r') as f:
                     lines = f.readlines()
@@ -125,11 +150,17 @@ def main(inp, mylabels_path='video_obj_detect'):
 
                         if cls == 0:  # 默认的coco.yaml中，0表示person这一类别
                             objs.append(xyxy)
+                            person_cnt += 1
+                        elif cls == 1:
+                            bicycle_cnt += 1
+                        elif cls == 3:
+                            motorcycle_cnt += 1
+                    # print(f'[debug] {txt_path} 人{person_cnt} 自{bicycle_cnt} 摩{motorcycle_cnt}')
 
             # temp_c = 0
 
-            # 统计每一帧内人的是否骑车
-            isCycler = []
+            # 统计每一帧内的骑车人数
+            cycler_cnt = 0
             
             for obj in objs:
                 # temp_c += 1
@@ -161,10 +192,9 @@ def main(inp, mylabels_path='video_obj_detect'):
                     knee = np_landmarks[mp_pose.PoseLandmark.LEFT_KNEE]
                     hip = np_landmarks[mp_pose.PoseLandmark.LEFT_HIP]
                     angle = cal_angle(ankle, knee, hip)
+
                     if angle < 140:
-                        isCycler.append([obj, True])
-                    else:
-                        isCycler.append([obj, False])
+                        cycler_cnt += 1
                         
                     info_list.append([idx+1, angle])
                 
@@ -179,7 +209,7 @@ def main(inp, mylabels_path='video_obj_detect'):
                 # if idx+1 == 28:
                 #     cv2.imwrite(f'test_28_frame_{temp_c}_after.jpg', frame)
 
-            addInfoText(frame, isCycler)
+            frame = addInfoText(frame, cycler_cnt, person_cnt, bicycle_cnt, motorcycle_cnt)
             source.show(frame)
             vid_writer.write(frame)
 
